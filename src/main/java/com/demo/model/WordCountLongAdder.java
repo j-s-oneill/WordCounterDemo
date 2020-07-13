94e6ed35f2725be3dd0e453c3ae63f1f81c42563
James ONeill <james.stephen.oneill@gmail.com>
2020-07-13 22:30:32 +0100

    updated with rwLock comments

--- src/main/java/com/demo/model/WordCountLongAdder.java
+++ src/main/java/com/demo/model/WordCountLongAdder.java
@@ -8,51 +8,55 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.demo.lang.Translator;
 
-
 public class WordCountLongAdder implements WordCounter {
 
-		private final Translator translator;
-	    private final ReentrantReadWriteLock RWLock = new ReentrantReadWriteLock();
-	    private final Lock readLock = RWLock.readLock();
-	    private final Lock writeLock = RWLock.writeLock();
+	private final Translator translator;
+	private final ReentrantReadWriteLock RWLock = new ReentrantReadWriteLock();
+	private final Lock readLock = RWLock.readLock();
+	private final Lock writeLock = RWLock.writeLock();
 
+	//please note:
+	//I would prefer to call 
+	// "translator.translate(word)" and validate(word)		 
+	// outside of this client code, and I can then remove the RWlock and let the HashMap and LongAdder 
+	// handle concurrent increments provide we pass a valid/translated word
+	// the spec stated I can only have one "add" and  one "count" method so unfortunately I have to wrap with a rwlock .
+	
 	public WordCountLongAdder(Translator translator) {
 		this.translator = translator;
 	}
-	
-    private final ConcurrentMap<String, LongAdder> map = new ConcurrentHashMap<>();
-    
-    
-   
-	public void add(String word) {
+
+	private final ConcurrentMap<String, LongAdder> map = new ConcurrentHashMap<>();
+
+	@Override
+	public void add(String word) {		
 		writeLock.lock();
 		try {
-		
-		word = translator.translate(word);
-		word = validate(word);	
-		map.computeIfAbsent(word,k-> new LongAdder()).increment();
-		}
-		finally {
+
+			word = translator.translate(word);		 
+			word = validate(word);
+			map.computeIfAbsent(word, k -> new LongAdder()).increment();
+		} finally {
 			writeLock.unlock();
 		}
-	
-		
+
 	}
 
-    public long count(String word){
-    	readLock.lock();
+	@Override
+	public long count(String word) {
+		readLock.lock();
 		try {
-    	word = translator.translate(word);
-    	word = validate(word);	
-    	LongAdder adder = map.get(word.toLowerCase());
-        if(adder != null){
-        	
-            return adder.sum();
-        }
-        return 0;
-    } finally {
-    	readLock.unlock();
+			word = translator.translate(word);
+			word = validate(word);
+			LongAdder adder = map.get(word.toLowerCase());
+			if (adder != null) {
+
+				return adder.sum();
+			}
+			return 0;
+		} finally {
+			readLock.unlock();
+		}
+
 	}
-    
-    }
 }
--- src/main/java/com/demo/model/WordCountTrie.java
+++ src/main/java/com/demo/model/WordCountTrie.java
@@ -1,5 +1,8 @@
 package com.demo.model;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import com.demo.lang.Dictionary;
 import com.demo.lang.Translator;
 
@@ -7,35 +10,54 @@ public class WordCountTrie implements WordCounter {
 
 	private final Translator translator;
 	private Node root; // root of trie
-	
+	private final ReentrantReadWriteLock RWLock = new ReentrantReadWriteLock();
+	private final Lock readLock = RWLock.readLock();
+	private final Lock writeLock = RWLock.writeLock();
+
 	public WordCountTrie(Translator translator) {
 		this.translator = translator;
 	}
 
+	@Override
 	public void add(String word) {
+		writeLock.lock();
+		try {
 		word = translator.translate(word);
 		word = validate(word);
-		put(word, get(word)+1);
+		put(word, get(word) + 1);
+		} finally {
+			writeLock.unlock();
+		}
 	}
 
+	@Override
 	public long count(String word) {
+		readLock.lock();
+		try {
 		word = validate(word);
 		return get(word);
+		} finally {
+			readLock.unlock();
+		}
 	}
 
 	private long get(String key) {
-		if (root == null) return 0;
+		if (root == null)
+			return 0;
 		Node node = get(root, key, 0); // find the key from the root node and 1st character.
-		if (node == null)	return 0;
+		if (node == null)
+			return 0;
 		return node.val;
 	}
 
 	private Node get(Node node, String key, int index) {
-		if (node == null)	return null;
-		if (index == key.length()) return node; //when we have the char_index= length of the key/word we return
-		
+		if (node == null)
+			return null;
+		if (index == key.length())
+			return node; // when we have the char_index= length of the key/word we return
+
 		char c = key.charAt(index);
-		return get(node.next[getIndex(c)], key, index + 1);  //move to the next node i.e. charater 2
+		return get(node.next[getIndex(c)], key, index + 1); // move to the next node i.e. charater 2
 	}
 
 	private void put(String key, long val) {
@@ -43,24 +65,26 @@ public class WordCountTrie implements WordCounter {
 	}
 
 	private Node put(Node node, String key, long val, int index) {
-		if (node == null)	node = new Node();
-		
+		if (node == null)
+			node = new Node();
+
 		if (index == key.length()) {
 			node.val = val;
 			return node;
 		}
 		char c = key.charAt(index);
 		node.next[getIndex(c)] = put(node.next[getIndex(c)], key, val, index + 1);
-		
+
 		return node;
 	}
 
 	private int getIndex(char character) {
-		return character -'a'; // return character - 97 , we subtract 97 to start at 0 in ASCII we can then have 26 letters
+		return character - 'a'; // return character - 97 , we subtract 97 to start at 0 in ASCII we can then
+								// have 26 letters
 	}
-	
+
 	private static class Node {
 		private long val;
-		private Node[] next = new Node[Dictionary.ALPHABET_SIZE]; 
+		private Node[] next = new Node[Dictionary.ALPHABET_SIZE];
 	}
 }
